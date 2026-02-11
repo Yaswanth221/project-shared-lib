d171f88b0faf0bf2ce6d7bf968b92b8fcf3465ce
D Yaswanth Prasad <ydustakar@gmail.com>
2026-02-11 16:36:18 +0530

    updated resources

--- vars/sonarConfig.groovy
+++ vars/sonarConfig.groovy
@@ -1,11 +1,5 @@
 def call() {
-  return [
-    enabled: true,
-    sonarqube: [
-      sonarqube_url: "http://sonarqube:9000"
-    ],
-    fail_build_qualitycheck: true,
-    skip_build: false,
-    fail_execution_failure: true
-  ]
+  echo "[sonarConfig] loading from resources/sonar_config.json"
+  def txt = libraryResource('sonar_config.json')
+  return readJSON(text: txt)
 }
